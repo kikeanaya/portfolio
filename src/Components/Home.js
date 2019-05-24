ce495ce94524f5aaaee3ef58aeb81c5d4434e0f9
kikeanaya <luiseanayam@gmail.com>
2019-05-24 14:21:26 +0200

    first commit, base established

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+
+    return (
+        <div className="homepage">
+            <h2>Hi, I'm Enrique</h2>
+            <p>I'm a Web Developer</p>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/Components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Menu = () => {
+
+    return (
+        <div className="header">
+            <h1>Enrique Anaya</h1>
+            <ul className="nav-bar">
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
